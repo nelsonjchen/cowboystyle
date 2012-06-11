e64011d8d78d0c5213c951442407d6b31c23d100
Nelson Chen <crazysim@gmail.com>
2012-06-11 01:55:27 -0700

    updated

--- deploy.py
+++ deploy.py
@@ -3,3 +3,8 @@ import ConfigParser
 
 print reddit.__version__
 print "loading config"
+
+config = ConfigParser.ConfigParser()
+username = config.get("user", username)
+password = config.get("user", password)
+subreddit = config.get("user", subreddit)
