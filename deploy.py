f8a59d763f234d0ad5526df0bcf78a29190bdff1
Nelson Chen <crazysim@gmail.com>
2012-06-11 02:01:39 -0700

    some auth

--- deploy.py
+++ deploy.py
@@ -4,7 +4,15 @@ import ConfigParser
 print reddit.__version__
 print "loading config"
 
+
 config = ConfigParser.ConfigParser()
-username = config.get("user", username)
-password = config.get("user", password)
-subreddit = config.get("user", subreddit)
+config.read(sys.argv[1])
+username = config.get("user", "username")
+password = config.get("user", "password")
+subreddit = config.get("user", "subreddit")
+
+USER_AGENT = "subreddit css bot for reddit %s" % subreddit
+
+with open("style.css",'r') as file:
+    style = file.read()
+
