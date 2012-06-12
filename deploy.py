795199a71f95043444723c5d18453934059dbf6a
Nelson Chen <crazysim@gmail.com>
2012-06-11 19:30:31 -0700

    robust deploy

--- deploy.py
+++ deploy.py
@@ -11,13 +11,23 @@ subreddit = config.get("user", "subreddit")
 
 USER_AGENT = "subreddit css bot for reddit %s" % subreddit
 
+print USER_AGENT
+
 with open("stylesheets/style.css",'r') as file:
     style = file.read()
 
+print "Going to Reddit"
 r = reddit.Reddit(user_agent = USER_AGENT)
 r.login(username, password)
 print "Logged in"
 sr = r.get_subreddit(subreddit)
 print "Got subreddit"
 sr.set_stylesheet(style)
-print "Finished uploading CSS to Subreddit"
+print "set style"
+style_set = sr.get_stylesheet()['stylesheet']
+if (style_set != style):
+    print "Style that was uploaded was invalid. Manual mode"
+    print style
+else:
+    print "Sucessfully uploaded CSS to Subreddit"
+
