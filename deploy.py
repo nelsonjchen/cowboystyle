a6728d0636986f59a54357f52b48cdad0c93fde9
Nelson Chen <crazysim@gmail.com>
2012-06-20 21:58:01 -0700

    use python 3

--- deploy.py
+++ deploy.py
@@ -1,9 +1,9 @@
 import sys
 import reddit
-import ConfigParser
+import configparser
 
-print "Beginning uploading CSS to Subreddit"
-config = ConfigParser.ConfigParser()
+print("Beginning uploading CSS to Subreddit")
+config = configparser.ConfigParser()
 config.read(sys.argv[1])
 username = config.get("user", "username")
 password = config.get("user", "password")
@@ -11,23 +11,23 @@ subreddit = config.get("user", "subreddit")
 
 USER_AGENT = "subreddit css bot for reddit %s" % subreddit
 
-print USER_AGENT
+print(USER_AGENT)
 
 with open("stylesheets/style.css",'r') as file:
     style = file.read()
 
-print "Going to Reddit"
+print("Going to Reddit")
 r = reddit.Reddit(user_agent = USER_AGENT)
 r.login(username, password)
-print "Logged in"
+print("Logged in")
 sr = r.get_subreddit(subreddit)
-print "Got subreddit"
+print("Got subreddit")
 sr.set_stylesheet(style)
-print "set style"
+print("set style")
 style_set = sr.get_stylesheet()['stylesheet']
 if (style_set != style):
-    print "Style that was uploaded was invalid. Manual mode"
+    print("Style that was uploaded was invalid. Manual mode")
     # print style
 else:
-    print "Sucessfully uploaded CSS to Subreddit"
+    print("Sucessfully uploaded CSS to Subreddit")
 
