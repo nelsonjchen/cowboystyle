5cc706acbe9d5c4f6d34ea010c4f69c6d8eb085d
Nelson Chen <crazysim@gmail.com>
2012-06-24 03:13:37 -0700

    updating deploy

--- deploy.py
+++ deploy.py
@@ -2,6 +2,9 @@ import sys
 import reddit
 import configparser
 import datetime
+import difflib
+from pprint import pprint
+import hashlib
 
 print("Beginning uploading CSS to Subreddit")
 config = configparser.ConfigParser()
@@ -23,11 +26,15 @@ r.login(username, password)
 print("Logged in")
 sr = r.get_subreddit(subreddit)
 print("Got subreddit")
-sr.set_stylesheet(style)
+style_hash = hashlib.md5(style.encode('UTF-8'))
+style = style + "/*" + style_hash + "*/"
 print("set style")
 style_set = sr.get_stylesheet()['stylesheet']
-if (style_set != style):
+if (style_set.find(style_hash) == -1):
     print("Style that was uploaded was invalid. Manual mode")
+    # d = difflib.Differ()
+    # result = list(d.compare(style_set.splitlines(1),style.splitlines(1)))
+    # pprint(result)
     # print style
 else:
     print("Sucessfully uploaded CSS to Subreddit")
