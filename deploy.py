fc36e96a9a8cb53b95786939b33fe3d67035c685
Nelson Chen <crazysim@gmail.com>
2012-07-26 15:30:21 -0700

    sets sidebar

--- deploy.py
+++ deploy.py
@@ -22,12 +22,18 @@ print(USER_AGENT)
 with open(filename, 'r') as file:
     style = file.read()
 
-print("Going to Reddit")
+with open('sidebar.mkdn', 'r') as file:
+    sidebar = file.read()
+
+print("Going to Reddit %s" % subreddit)
 r = praw.Reddit(user_agent = USER_AGENT)
 r.login(username, password)
 print("Logged in")
 sr = r.get_subreddit(subreddit)
 print("Got subreddit")
+print("Setting sidebar markdown")
+sr.update_settings(description = sidebar)
+print("hashing style")
 style_hash = hashlib.md5(style.encode('UTF-8'))
 style = style + "/*" + style_hash.hexdigest() + "*/"
 print("set style")
@@ -39,5 +45,5 @@ if (style_set.find(style_hash.hexdigest()) == -1):
     result = list(d.compare(style_set.splitlines(1),style.splitlines(1)))
     pprint(result)
 else:
-    print("Sucessfully uploaded CSS to Subreddit")
+    print("Sucessfully uploaded CSS to Subreddit %s" % subreddit)
 print(datetime.datetime.now().ctime())
